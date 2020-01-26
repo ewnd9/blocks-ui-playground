05e350689285c706adfa1fd818aab2ae3ba55660
ewnd9 <ewndnine@gmail.com>
2020-01-26 10:54:27 +0300

    save state in url hash

--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,63 @@
 import React from 'react';
+import qs from 'qs';
 import BlocksEditor from 'blocks-ui';
 import * as defaultBlocks from '@blocks/react';
+import debounce from 'lodash/debounce';
+
 import { HeaderOther } from './blocks/header-other-block';
 import { CardBlock } from './blocks/material/card/card-block';
 
-const JSX = `
-import React from 'react'
-import { HeaderBasic } from '@blocks/blocks'
-
-export default () => (
-  <Blocks.Root>
-    <HeaderBasic>
-      <HeaderBasic.Logo to="/">Hello</HeaderBasic.Logo>
-      <HeaderBasic.Nav>
-        <HeaderBasic.Link to="/about">About</HeaderBasic.Link>
-        <HeaderBasic.Link to="/blog">Blog</HeaderBasic.Link>
-        <HeaderBasic.Link to="/contact">Contact</HeaderBasic.Link>
-      </HeaderBasic.Nav>
-    </HeaderBasic>
-  </Blocks.Root>
-)
-`;
-
+const JSX = getInitialJSX();
 const customBlocks = {
   HeaderOther,
   CardBlock
 };
 const blocks = { ...defaultBlocks, ...customBlocks };
+const setHashDebounced = debounce(hash => {
+  location.hash = hash;
+});
 
 export const Editor = () => (
   <BlocksEditor
     src={JSX}
     blocks={blocks}
-    onChange={code => console.log(code)}
+    onChange={code => {
+      const hash = qs.stringify({code: encodeURIComponent(code)});
+
+      if (hash !== location.hash) {
+        setHashDebounced(hash);
+      }
+    }}
   />
 );
+
+function getInitialJSX() {
+  try {
+    const params = qs.parse(location.hash.slice(1));
+    const code = params.code && decodeURIComponent(params.code);
+
+    if (code) {
+      return code;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  return `
+      import React from 'react'
+      import { HeaderBasic } from '@blocks/blocks'
+
+      export default () => (
+        <Blocks.Root>
+          <HeaderBasic>
+            <HeaderBasic.Logo to="/">Hello</HeaderBasic.Logo>
+            <HeaderBasic.Nav>
+              <HeaderBasic.Link to="/about">About</HeaderBasic.Link>
+              <HeaderBasic.Link to="/blog">Blog</HeaderBasic.Link>
+              <HeaderBasic.Link to="/contact">Contact</HeaderBasic.Link>
+            </HeaderBasic.Nav>
+          </HeaderBasic>
+        </Blocks.Root>
+      )
+    `;
+}
